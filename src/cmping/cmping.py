a2f21a3335bf7d8abc5ef08e9d25155d5c2a1036
holger krekel <holger@merlinux.eu>
2025-11-21 22:51:47 +0100

    0.4.2 -- cache addresses/accounts properly, reuse them across sessions, and create new addresses as needed

--- src/cmping/cmping.py
+++ src/cmping/cmping.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 import time
 
-from deltachat_rpc_client import DeltaChat, Rpc
+from deltachat_rpc_client import DeltaChat, EventType, Rpc
 
 
 def main():
@@ -25,6 +25,20 @@ def main():
     perform_ping(args.relay1, args.relay2)
 
 
+def get_relay_account(dc, domain):
+    for account in dc.get_all_accounts():
+        if account.get_config("addr").split("@")[1] == domain:
+            account.bring_online()
+            return account
+
+    print(f"creating account on {domain}")
+    account = dc.add_account()
+    account.set_config("mdns_enabled", "0")
+    account.set_config_from_qr(f"dcaccount:{domain}")
+    account.bring_online()
+    return account
+
+
 def perform_ping(relay1, relay2):
     with Rpc() as rpc:
         dc = DeltaChat(rpc)
@@ -34,37 +48,28 @@ def perform_ping(relay1, relay2):
             file=sys.stderr,
         )
 
-        if len(dc.get_all_accounts()) != 2:
-            assert len(dc.get_all_accounts()) == 0, "remove accounts directory"
-            print(f"recreating accounts on {relay1} and {relay2}")
-            ac1 = dc.add_account()
-            ac2 = dc.add_account()
-            ac1.set_config("mdns_enabled", "0")
-            ac2.set_config("mdns_enabled", "0")
-            ac1.set_config_from_qr(f"dcaccount:{relay1}")
-            ac2.set_config_from_qr(f"dcaccount:{relay2}")
-        else:
-            ac1, ac2 = dc.get_all_accounts()
+        sender = get_relay_account(dc, relay1)
+        receiver = get_relay_account(dc, relay2)
+        chat1 = sender.create_chat(receiver)
+        _chat2 = receiver.create_chat(sender)
 
-        ac1.bring_online()
-        ac2.bring_online()
-
-        chat1 = ac1.create_chat(ac2)
-        _chat2 = ac2.create_chat(ac1)
-
-        addr1, addr2 = ac1.get_config("addr"), ac2.get_config("addr")
+        addr1, addr2 = sender.get_config("addr"), receiver.get_config("addr")
         print(f"PING {relay1} ({addr1}) -> {relay2} ({addr2}))")
         for i in range(60):
             text = f"ping {i:59}"
             start = time.time()
             chat1.send_text(text)
             while 1:
-                msg = ac2.wait_for_incoming_msg().get_snapshot()
-                if msg.text == text:
-                    break
-                print(f"received historic/bogus message from {relay2}: ignoring")
+                event = receiver.wait_for_event()
+                if event.kind == EventType.INCOMING_MSG:
+                    msg = receiver.get_message_by_id(event.msg_id)
+                    if msg.get_snapshot().text == text:
+                        break
+                    print(f"received historic/bogus message from {addr2}: {msg.text}")
+                elif event.kind == EventType.ERROR:
+                    print(f"ERROR: {event.msg}")
             print(
-                f"{len(text)} bytes [ME] -> {relay1} -> {relay2} -> [ME] seq={i} time={time.time() - start:.4}s"
+                f"{len(text)} bytes ME -> {relay1} -> {relay2} -> ME seq={i} time={time.time() - start:.2}s"
             )
             time.sleep(1)
 
