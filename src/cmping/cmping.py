1b6a86d5a27cf2a76d4e5bb0eb0b81f592b3761d
holger krekel <holger@merlinux.eu>
2025-11-21 18:54:25 +0100

    snap

--- src/cmping/cmping.py
+++ src/cmping/cmping.py
@@ -2,28 +2,25 @@ import argparse
 import logging
 import sys
 
-from deltachat_rpc_client import DeltaChat, EventType, Rpc, events
+from deltachat_rpc_client import DeltaChat, EventType, Rpc, events, run_bot_cli
 
 
-def main(args=None):
-    if args is None:
-        args = list(sys.argv)
-
+def main():
     parser = argparse.ArgumentParser(
         description="ping between addresses of chatmail relays"
     )
 
     parser.add_argument(
-        dest="relay1",
+        "relay1",
         action="store",
         help="chatmail relay domain",
     )
     parser.add_argument(
-        dest="relay2",
+        "relay2",
         action="store",
         help="chatmail relay domain",
     )
-    args = parser.parse_args(args=args)
+    args = parser.parse_args()
 
     perform_ping(args.relay1, args.relay2)
 
@@ -48,11 +45,14 @@ def log_error(event):
     logging.error(event.msg)
 
 
-@hooks.on(events.NewMessage(func=lambda e: not e.command))
+@hooks.on(events.NewMessage())
 def echo(event):
     snapshot = event.message_snapshot
-    if snapshot.text or snapshot.file:
-        snapshot.chat.send_message(text=snapshot.text, file=snapshot.file)
+    if snapshot.text:
+        if not snapshot.text.startswith("> "):
+            snapshot.chat.send_message(text=f"> {snapshot.text}")
+        else:
+            print(f"ECHO FROM {snapshot.sender}")
 
 
 def perform_ping(relay1, relay2):
@@ -60,6 +60,7 @@ def perform_ping(relay1, relay2):
         deltachat = DeltaChat(rpc)
         system_info = deltachat.get_system_info()
         log_verbose("Running deltachat core %s", system_info.deltachat_core_version)
+        log_verbose("systeminfo %s", system_info)
 
         accounts = deltachat.get_all_accounts()
         if len(accounts) == 0:
@@ -67,12 +68,22 @@ def perform_ping(relay1, relay2):
             sender.set_config_from_qr(f"dcaccount:https://{relay1}/new")
             receiver = deltachat.add_account()
             receiver.set_config_from_qr(f"dcaccount:https://{relay2}/new")
-            assert 0
+            accounts = [sender, receiver]
+        assert len(accounts) == 2, "remove accounts folder"
+        for client in accounts:
+            if not client.is_configured():
+                configure_thread.
+                configure_thread.start()
+        sender, receiver = accounts
 
-        assert len(accounts) == 2, len(accounts)
+        i = 0
+        while True:
+            sender.send_text(str(0))
+            time.sleep(1)
+            i += 1
 
-        account1, account2 = deltachat.get_all_accounts()
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     main()
