42438444684f047810b4ba0e145910440724b252
holger krekel <holger@merlinux.eu>
2025-11-21 20:18:48 +0100

    snap3

--- src/cmping/cmping.py
+++ src/cmping/cmping.py
@@ -1,6 +1,9 @@
+import string
 import argparse
 import logging
 import sys
+import secrets
+import time
 
 from deltachat_rpc_client import DeltaChat, EventType, Rpc, events, run_bot_cli
 
@@ -25,63 +28,47 @@ def main():
     perform_ping(args.relay1, args.relay2)
 
 
-def log_verbose(msg, args):
-    print(msg % args)
+ALPHANUMERIC = string.ascii_lowercase + string.digits
+ALPHANUMERIC_PUNCT = string.ascii_letters + string.digits + string.punctuation
 
-
-hooks = events.HookCollection()
-
-
-@hooks.on(events.RawEvent)
-def log_event(event):
-    if event.kind == EventType.INFO:
-        logging.info(event.msg)
-    elif event.kind == EventType.WARNING:
-        logging.warning(event.msg)
-
-
-@hooks.on(events.RawEvent(EventType.ERROR))
-def log_error(event):
-    logging.error(event.msg)
-
-
-@hooks.on(events.NewMessage())
-def echo(event):
-    snapshot = event.message_snapshot
-    if snapshot.text:
-        if not snapshot.text.startswith("> "):
-            snapshot.chat.send_message(text=f"> {snapshot.text}")
-        else:
-            print(f"ECHO FROM {snapshot.sender}")
+def newmail():
+    user = "".join(random.choices(ALPHANUMERIC, 9)
+    password = "".join(
+        secrets.choice(ALPHANUMERIC_PUNCT)
+        for _ in range(25)
+    )
+    return dict(email=f"{user}@{config.mail_domain}", password=f"{password}")
 
 
 def perform_ping(relay1, relay2):
     with Rpc() as rpc:
-        deltachat = DeltaChat(rpc)
-        system_info = deltachat.get_system_info()
-        log_verbose("Running deltachat core %s", system_info.deltachat_core_version)
-        log_verbose("systeminfo %s", system_info)
+        dc = DeltaChat(rpc)
+        system_info = dc.get_system_info()
+        print(f"Running deltachat core {system_info.deltachat_core_version}", file=sys.stderr)
 
-        accounts = deltachat.get_all_accounts()
-        if len(accounts) == 0:
-            sender = deltachat.add_account()
-            sender.set_config_from_qr(f"dcaccount:https://{relay1}/new")
-            receiver = deltachat.add_account()
-            receiver.set_config_from_qr(f"dcaccount:https://{relay2}/new")
-            accounts = [sender, receiver]
-        assert len(accounts) == 2, "remove accounts folder"
-        for client in accounts:
-            if not client.is_configured():
-                configure_thread.
-                configure_thread.start()
-        sender, receiver = accounts
+        ac1 = dc.add_account()
+        ac2 = dc.add_account()
 
-        i = 0
-        while True:
-            sender.send_text(str(0))
-            time.sleep(1)
-            i += 1
+        ac1.set_config("mdns_enabled", "0")
+        ac2.set_config("mdns_enabled", "0")
+        ac1.set_config("addr", f"ci_{ac1.get_config('addr')[3:]}")
+        ac2.set_config("addr", f"ci_{ac2.get_config('addr')[3:]}")
 
+        ac1.bring_online()
+        ac2.bring_online()
+
+        chat1 = ac1.create_chat(ac2)
+        chat2 = ac2.create_chat(ac1)
+
+        addr1 = ac1.get_config("configured_addr")
+        addr2 = ac2.get_config("configured_addr")
+        print(f"PING {relay1} ({addr1}) -> {relay2} ({addr2}))")
+        for i in range(60):
+            text = f"ping {i:59}"
+            start = time.time()
+            chat1.send_text(text)
+            assert ac2.wait_for_incoming_msg().get_snapshot().text == text
+            print(f"{len(text)} bytes from {relay2} to {relay2} seq={i} time={time.time() - start}")
 
 
 if __name__ == "__main__":
